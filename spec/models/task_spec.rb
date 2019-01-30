9c8b54f5b6851aa946a8d2bbb8893ae788724038
Christopher Choi <cdddchris@gmail.com>
2019-01-30 20:21:25 +0900

    Fix method, was missing a ? for method

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -10,7 +10,29 @@ RSpec.describe 'Task' do
   end
 
   it 'allows us to complete a task' do
-    task.mark_complete
+    task.mark_completed
     expect(task).to be_complete
+  end
+end
+
+describe 'velocity' do
+  let(:task) { Task.new(size: 3) }
+
+  it 'does not count an incomplete task toward velocity' do
+    expect(task).not_to be_part_of_velocity
+  end
+
+  it 'counts a recently completed tak toward velocity' do
+    task.mark_completed(1.day.ago)
+
+    expect(task).to be_part_of_velocity
+    expect(task.points_toward_velocity).to eq(3)
+  end
+
+  it 'does not count a long-ago completed task toward velocity' do
+    task.mark_completed(6.months.ago)
+
+    expect(task).not_to be_part_of_velocity
+    expect(task.points_toward_velocity).to eq(0)
   end
 end
